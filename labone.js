c4a57942691be9f6579963652efdf13a33300469
Mohab <34834902+mohab2017@users.noreply.github.com>
2018-01-06 16:37:17 +0200

    Add files via upload

---
+++ labone.js
@@ -0,0 +1,17 @@
+//function fizz_buzz(num){
+  if(num%3==0 && num%5==0)
+    console.log("fizz buzz");
+  else if(num%5==0)
+    console.log("buzz");
+  else if (num%3==0)
+    console.log("fizz");
+}
+
+fizz_buzz(15)//
+
+function random_machine(array)
+{
+  elementnum=array.length;
+  Math.floor((Math.random() *elementnum + 1);
+}
+  random_machine(5,4,3,)           
